a6505e972433cc289e18aaf31c5fcd744cb7790e
Devansh Batra <devanshbatra04@gmail.com>
2018-06-28 18:46:33 +0530

    Signup form now complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Successfully Signed Up #{@user.username}"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :name, :description)
+  end
 end
