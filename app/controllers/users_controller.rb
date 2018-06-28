757564bcc7dbaca8537575191dd17534b72978e3
Devansh Batra <devanshbatra04@gmail.com>
2018-06-28 19:03:05 +0530

    Edit route working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,16 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+
+  end
   def edit
     @user = User.find(params[:id])
   end
